9f8a2741d949e2388f6b0761c638cbdea088dda1
Pechenin.NA <n.pechenin@socialquantum.com>
2019-01-14 22:18:30 +0300

    Add canvas UI scene

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Импорт инфраструктуры для работы с кодом UI.
+
+public class UIController : MonoBehaviour {
+
+    // Объект сцены Reference Text, предназначенный для задания свойства text.
+    [SerializeField] private Text scoreLabel;
+
+    // Update is called once per frame
+    void Update() {
+        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+    }
+
+    public void OnOpenSettings() {
+        // Метод, вызываемый кнопкой настроек.
+        Debug.Log("open settings");
+    }
+    
+    public void OnPointerDown() {
+        Debug.Log("pointer down");
+    }
+}
