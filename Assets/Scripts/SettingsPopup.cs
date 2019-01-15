c5e3d24988062713afc43a6b5de34e5abf9a2f71
Pechenin.NA <n.pechenin@socialquantum.com>
2019-01-15 12:15:57 +0300

    Add new popup window

---
+++ Assets/Scripts/SettingsPopup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPopup : MonoBehaviour {
+    public void Open() {
+//        Активируйте этот объект, чтобы открыть окно.
+        gameObject.SetActive(true);  
+    }
+    
+    public void Close() {
+//        Деактивируйте объект, чтобы закрыть окно.
+        gameObject.SetActive(false);  
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,6 +7,13 @@ public class UIController : MonoBehaviour {
 
     // Объект сцены Reference Text, предназначенный для задания свойства text.
     [SerializeField] private Text scoreLabel;
+    [SerializeField] private SettingsPopup settingsPopup;
+    
+    void Start() {
+//        Закрываем всплывающее окно в момент начала игры.
+        settingsPopup.Close();
+        
+    }
 
     // Update is called once per frame
     void Update() {
@@ -16,6 +23,8 @@ public class UIController : MonoBehaviour {
     public void OnOpenSettings() {
         // Метод, вызываемый кнопкой настроек.
         Debug.Log("open settings");
+//        Заменяем отладочный текст методом всплывающего окна.
+        settingsPopup.Open(); 
     }
     
     public void OnPointerDown() {
